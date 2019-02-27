f5fd3b6978b0b9a11cf0063b06cccfd4b63f65ca
Viraj Trivedi <inf3cti0n95@gmail.com>
2019-02-27 23:44:53 +0530

    chore(docs): changed basic configurations

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,27 +11,27 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: 'inf3cti0n95',
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
+    image: 'https://avatars0.githubusercontent.com/u/13603045?s=460&v=4',
+    infoLink: 'https://github.com/inf3cti0n95',
     pinned: true,
   },
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'sidekik', // Title for your website.
+  tagline: 'Sidekik for all Data Structures and Algorithms you need.',
+  url: 'https://inf3cti0n95.github.io', // Your website URL
+  baseUrl: '/sidekik/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'sidekik',
+  organizationName: 'inf3cti0n95',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'

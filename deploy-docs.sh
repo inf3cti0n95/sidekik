580e58e2cf8499f810e0f17d50dcb2d2a7fff073
Viraj Trivedi <inf3cti0n95@gmail.com>
2019-03-04 13:08:23 +0530

    chore(deploy-docs.sh): copying docs from individual package to docs/ directory .
    Collecting docs from all the packages inside the packages/ directory and copying them into the main
    'docs' directory for docusaurus to use them.
    
    ISSUES CLOSED: #19

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -6,9 +6,20 @@ TARGET_BRANCH="gh-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy."
+    echo "Skipping documentation deployment."
     exit 0
 fi
+
+# Collect Docs from package/*/docs/ directory
+echo Collecting Docs from "package/*/docs/*.md"
+for dir in $(ls packages/* -d);
+    do 
+        for doc in $(ls $dir/docs/*.md);
+            do
+                cp $doc $PWD/docs/$(basename $dir)-$(basename $doc)
+                echo Copied $dir/docs/$(basename $doc) to docs/$(basename $dir)-$(basename $doc) 
+            done
+    done
 
 # Deploy Docs
 git config --global user.name "${GH_NAME}"

0c34770910301355103981e063aff66a322febb8
Viraj Trivedi <inf3cti0n95@gmail.com>
2019-03-26 21:52:12 +0530

    fix: removed debugging code (#29)

--- packages/binary-search-tree/src/index.ts
+++ packages/binary-search-tree/src/index.ts
@@ -1,13 +1,2 @@
-import { BinaryNode } from "./BinaryNode";
-import { BinarySearchTree } from "./BinarySearchTree";
-
-let x = new BinaryNode<number>(6);
-let y = new BinarySearchTree<number>((a, b) => a.key < b.key);
-
-y.insert(new BinaryNode(1));
-y.insert(new BinaryNode(2));
-y.insert(new BinaryNode(4));
-y.insert(new BinaryNode(-1));
-y.insert(x);
-
-console.log(y);
+export { BinaryNode } from "./BinaryNode";
+export { BinarySearchTree } from "./BinarySearchTree";
